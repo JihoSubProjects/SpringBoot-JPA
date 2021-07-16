6f8b4cb935162a7a81ced4379d5c93ce286fd8e4
jiho <jiho@kakao.com>
2021-07-16 12:09:17 +0900

    Fix annotation - Add @Repository

--- src/main/java/kim/jiho/jpa/db/repository/UserRepository.java
+++ src/main/java/kim/jiho/jpa/db/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package kim.jiho.jpa.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import kim.jiho.jpa.db.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {}
